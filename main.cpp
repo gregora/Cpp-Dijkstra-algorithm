49ad8ac576e0e4515f3c2de33bbe27a53e9b75c8
Gregor A <gregor.alic@gimb.org>
2020-02-26 15:48:21 +0100

    Add functionality (returns distance)

--- main.cpp
+++ main.cpp
@@ -1,75 +1,149 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <limits>
 
-std::string input = "start";
+std::vector <int> edges; // {vertex1, vertex2, v1 -> v2 distance, vertex3, vertex4, v3 -> v4 distance} - vertex2 is connected with vertex 1 with distance v1 -> v2 ...
+int vertices;
+std::string inp1, inp2, inp3;
 
-int counter = 0;
+int startv;
+int endv;
 
-int toPower(int number, int power){
+int MAX_INT = std::numeric_limits<int>::max();
 
-  int ognumber = number;
-  if(power == 0) return 1;
-
-  for(int x = 1; x < power; x++){
-    number *= ognumber;
-  }
-
-  return number;
-
-}
-
-
-int stringToInteger(std::string str){
-
-  char cchar;
-  int cint;
-  int sum = 0;
-
-  for(int x = 0; x < str.size(); x++){
-
-    cchar = str[x];
-
-    if(cchar == '0'){
-    }else if (cchar == '1'){
-      cint = 0;
-      cint = 1;
-    }else if (cchar == '2'){
-      cint = 2;
-    }else if (cchar == '3'){
-      cint = 3;
-    }else if (cchar == '4'){
-      cint = 4;
-    }else if (cchar == '5'){
-      cint = 5;
-    }else if (cchar == '6'){
-      cint = 6;
-    }else if (cchar == '7'){
-      cint = 7;
-    }else if (cchar == '8'){
-      cint = 8;
-    }else if (cchar == '9'){
-      cint = 9;
-    }else{
-      cint = 0;
-    }
-
-    sum += toPower(10, str.size() - x - 1)*cint;
-
-  }
-
-  return sum;
-
-}
+std::vector <int> queue; // {vertexid, last id, distance}
 
 int main(){
 
-  std::cout << toPower(3, 3) << std::endl;
+  //get vertices number and create solved array
+  std::cout << "How many vertices are in your graph? ";
+  std::cin >> inp1;
+  vertices = std::stoi(inp1);
+  int solved[vertices + 1][2]; // {min-distance, last vertex id}
+  for(int fill = 0; fill < vertices + 1; fill++){
+    solved[fill][0] = -1;
+    solved[fill][1] = -1;
+  }
 
-  while(input != "end"){
+  std::cout << "From which vertex do you want to start, and to which vertex do you want to come (0 [zero] is not valid)? ";
+  std::cin >> inp1 >> inp2; // vertex1 id, vertex2 id
 
-    std::cin >> input;
+  startv = std::stoi(inp1); //starting vertex id
+  endv = std::stoi(inp2); //ending vertex id
+
+  while (true){
+
+    std::cin >> inp1 >> inp2 >> inp3;
+
+    if(inp1 != "end" && inp2 != "end" && inp3 != "end"){
+
+      edges.push_back(std::stoi(inp1)); //vertex1
+      edges.push_back(std::stoi(inp2)); //vertex2
+      edges.push_back(std::stoi(inp3)); //distance
+
+    }else{
+      break;
+    }
 
   }
+
+  solved[startv][0] = 0;
+  solved[startv][1] = 0;
+
+  //END input, start solving
+
+  //push first queue
+  for(int x = 0; x < edges.size(); x = x + 3){
+    if(edges.at(x) == startv){
+      queue.push_back(edges.at(x + 1));
+      queue.push_back(edges.at(x));
+      queue.push_back(edges.at(x + 2));
+    }
+  }
+
+
+  int lastId = startv;
+
+  //start the algorithm
+  while(queue.size() > 0){
+
+    int minid;
+    int minvalue = MAX_INT;
+    int eraseid;
+
+    //find the smallest queue member
+    for(int y = 0; y < queue.size(); y = y + 3){
+
+      if(minvalue > queue.at(y + 2)){
+        minvalue = queue.at(y + 2);
+        minid = queue.at(y);
+        eraseid = y;
+      }
+
+    }
+
+    //set smallest queue member to solved
+    solved[minid][0] = minvalue;
+    solved[minid][1] = lastId; //probably a bug
+
+    int curDist = queue.at(eraseid + 2);
+
+    //remove from queue
+    queue.erase(queue.begin() + eraseid);
+    queue.erase(queue.begin() + eraseid);
+    queue.erase(queue.begin() + eraseid);
+
+
+    //add all its neighbours to the queue
+    for(int z = 0; z < edges.size() - 2; z = z + 3){
+
+      if(edges.at(z) == minid){
+        //found neighbour
+
+        int neighbourId = edges.at(z + 1);
+        if(solved[neighbourId][1] == -1){
+          //neighbour is not solved yet
+
+          bool found = false;
+          for(int f = 0; f < queue.size(); f = f + 3){
+            //is neighbour in the queue?
+
+            if(queue.at(f) == neighbourId){
+              found = true;
+              //yes it is
+              if(queue.at(f + 2) > curDist + edges.at(z + 2)){
+                queue.at(f + 2) = curDist + edges.at(z + 2);
+                queue.at(f + 1) = minid;
+
+              }
+
+              break;
+            }
+
+          }
+
+          if(!found){
+
+            queue.push_back(neighbourId);
+            queue.push_back(minid);
+            queue.push_back(curDist + edges.at(z + 2));
+
+
+          }
+
+        }
+      }
+
+
+    }
+
+
+
+  }
+
+  std::cout << "---- Finished with calculations ----" << std::endl;
+  std::cout << solved[endv][0] << std::endl;
+
 
 }
